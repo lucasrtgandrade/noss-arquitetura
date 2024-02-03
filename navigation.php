6883410dded4ed964e8a68786ab266d6581b390e
Lucas Reategui <lucasrtgandrade@gmail.com>
2024-02-02 23:14:51 -0300

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,42 @@
+<header>
+    <div class="main-header header-style">
+        <div class="empty"></div>
+        <div class="hamburger-resize">
+            <button class="mobile-nav-toggle" aria-controls="primary-navigation" aria-expanded="false">
+                <span class="sr-only">Menu</span>
+            </button>
+        </div>
+        <div class="main-logo">
+            <a class="main-logo" href="../index.php">NOSS</a>
+        </div>
+        <div class="main-icons">
+            <a href="https://www.instagram.com/nossarquitetura/" target="_blank" aria-label="Entre no instagram da NOSS e conheça mais projetos">
+                <i class="fa-brands fa-instagram main-instagram"></i>
+            </a>
+            <a href="https://wa.me/5511957848872" target="_blank"><i class="fa-brands fa-whatsapp main-whatsapp" aria-label="Entre em contato pelo WhatsApp"></i></a>
+        </div>
+    </div>
+    <nav id="primary-navigation" class="primary-navigation" data-visible="false">
+        <div class="mobile-empty"></div>
+        <ul class="main-navigation nobullets">
+            <li><a class="nav-links-color" href="../index.php">Home</a></li>
+            <li><a class="nav-links-color" href="../paginas/sobre.php">Sobre</a></li>
+            <li><a class="nav-links-color" href="../paginas/projetos.php">Projetos</a></li>
+            <li><a class="nav-links-color" href="../paginas/contato.php">Contato</a></li>
+        </ul>
+        <div class="mobile-info">
+            <div class="mobile-logo">NOSS</div>
+            <div class="mobile-icons">
+                <a href="https://www.instagram.com/nossarquitetura/">
+                    <i class="fa-brands fa-instagram mobile-instagram" target="_blank" aria-label="Entre no instagram da NOSS e conheça mais projetos"></i>
+                </a>
+                <a href="https://wa.me/5511957848872" target="_blank"><i class="fa-brands fa-whatsapp mobile-whatsapp" aria-label="Entre em contato pelo WhatsApp"></i></a>
+            </div>
+            <address class="bottom-space">
+                Alameda Barão de Limeira, 1306 cj.43 <br>
+                São Paulo - SP
+            </address>
+        </div>
+        </div>
+    </nav>
+</header>
